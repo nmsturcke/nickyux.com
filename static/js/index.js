bc7e4f85729d70a0482f59583347d95027a1b8f7
nmsturcke <me@nickyux.com>
2025-10-08 13:22:33 +0200

    feat: re-theme

--- static/js/index.js
+++ static/js/index.js
@@ -1,234 +1,294 @@
-function setupBackground() {
-    const background = document.getElementById("background");
+gsap.registerPlugin(ScrollTrigger);
 
-    // Choose a random number between 0 and 6
-    const randomNumber = Math.floor(Math.random() * 7);
-    const child = background.children[randomNumber];
+let horizontalTween;
+const textShadowColor = "rgba(245, 245, 245, 0.2)"; //"#031d15";
 
-    if (!child) return;
-
-    child.classList.remove("d-none");
-
-    // Start at a random position
-    const randomX = Math.random() * (window.innerWidth - child.clientWidth);
-    const randomY = Math.random() * (window.innerHeight - child.clientHeight);
-    const randomRotation = (Math.random() - 0.5) * 60; // Random rotation between -30 and 30 degrees
-    child.style.transform = `translate(${randomX}px, ${randomY}px) rotate(${randomRotation}deg)`;
-
-    // // Animate the background element to move slowly around the screen
-    // gsap.to(child, {
-    //     x: `+=${(Math.random() - 0.5) * 200
-    //     }`, // Move randomly in the x direction
-    //     y: `+=${(Math.random() - 0.5) * 200
-    //     }`, // Move randomly in the y direction
-    //     rotate: `+=${(Math.random() - 0.5) * 20
-    //     }`, // Rotate randomly
-    //     duration: 20 + Math.random() * 10, // Random duration between 20 and 30 seconds
-    //     ease: "power1.inOut",
-    //     repeat: -1, // Repeat indefinitely
-    //     yoyo: true // Move back to the original position
-    // });
-
-
-    // Move with mouse
-    document.addEventListener("mousemove", (event) => {
-        const mouseX = event.clientX;
-        const mouseY = event.clientY;
-        const centerX = window.innerWidth / 2;
-        const centerY = window.innerHeight / 2;
-        const deltaX = (mouseX - centerX) / centerX; // -1 to 1
-        const deltaY = (mouseY - centerY) / centerY; // -1 to 1
-        
-        // Apply parabolic smoothing: square the values to create a curve
-        // This makes small movements even smaller and large movements more pronounced
-        const smoothDeltaX = Math.sign(deltaX) * Math.pow(Math.abs(deltaX), 2);
-        const smoothDeltaY = Math.sign(deltaY) * Math.pow(Math.abs(deltaY), 2);
-        
-        const moveAmount = 20; // Max movement in pixels
-        const newX = randomX + smoothDeltaX * moveAmount;
-        const newY = randomY + smoothDeltaY * moveAmount;
-        child.style.transform = `translate(${newX}px, ${newY}px) rotate(${randomRotation}deg)`;
+function animateStartup(withSliding = false, allPurple = false) {
+    const timeline = gsap.timeline({
+        // scrollTrigger: {
+        //     trigger: ".text-title",
+        //     start: "top top",
+        //     end: "+=3000",
+        //     scrub: 1,
+        //     pin: true,
+        //     anticipatePin: 1,
+        // }
     });
+    const fontsAndColors = [
+        ["Achafont", "#008b7b"],
+        ["Austria", "#09817f"],
+        ["BodoniFLF", "#127784"],
+        ["ERTHQAKE", "#1b6d88"],
+        ["FadeToGrey", "#24628d"],
+        ["Funkrocker", "#2d5891"],
+        ["Germanica", "#374e96"],
+        ["GlassHouses", "#40449a"],
+        ["KOMTIT", "#493a9e"],
+        ["newcomictitle", "#5230a3"],
+        ["Subtitle", "#5b25a7"],
+        ["TitleWaveRegular", "#641bac"],
+        ["Federal Escort", "#6D11B0"],
+    ];
 
-}
-
-function animateStartup() {
-    const timeline = gsap.timeline();
-
-    timeline.from(".text-supertitle.self-end:not(.after)", {
-        marginRight: "80vw",
-        duration: 2,
-        ease: "power3.out",
-        delay: 0.5
-    });
-    timeline.from(".text-title:not(.after)", {
-        y: 100,
-        opacity: 0,
-        duration: 1.5,
-        ease: "power3.out"
-    }, "-=1.5");
-    timeline.to(".text-supertitle:not(.after)", {
-        maxHeight: "0px",
-        opacity: 0,
-        duration: 0.5,
-        ease: "power3.inOut",
-    }, "goUp +=0.5");
-    timeline.to(".text-title:not(.after)", {
-        y: -1000,
-        opacity: 0,
-        duration: 1,
-        ease: "power3.in",
-    }, "goUp+=0.5");
-    timeline.to("div:has(> .text-title:not(.after))", {
-        maxHeight: "0px",
-        opacity: 0,
-        padding: "0px!important",
-        duration: 1,
-        ease: "power3.inOut"
-    }, "goUp+=1");
-    // }, "goUp+=2.5");
-    timeline.to("div:has(> .text-title.after)", {
-        y: 0,
-        opacity: 1,
-        duration: 2.5,
-        ease: "power3.out"
-    }, "goUp+=1");
-    timeline.from(".text-supertitle.after", {
-        y: -250,
-        duration: 2.5,
-        ease: "power3.out"
-    }, "goUp+=2.5");
-    timeline.to(".text-title.after", {
-        transform: "scale(1)",
-        duration: 2.5,
-        ease: "power3.out"
-    }, "goUp+=2.5");
-}
-
-function animateStartupAlternative() {
-    document.querySelector("#animation-primary").remove();
-    const timeline = gsap.timeline();
-
+    if (allPurple) {
+        document.querySelectorAll("#hero .text-title:not(.text-stone-200)").forEach(el => {
+            el.classList.add("!text-stone-200");
+        });
+    }
+        
     /* Move AM to the right */
-    timeline.from(".text-supertitle.self-end", {
+    timeline.from("#hero .text-supertitle.self-end", {
         marginRight: "80vw",
         duration: 2,
         ease: "power3.out",
         delay: 0.5
     });
     /* Show all titles */
-    timeline.from(".text-title:not(.after)", {
+    timeline.from("#hero .text-title", {
         y: 100,
         opacity: 0,
         duration: 1.5,
         ease: "power3.out"
     }, "-=1.5");
     /* Hide supertitle */
-    timeline.to(".text-supertitle", {
+    timeline.to("#hero .text-supertitle", {
         maxHeight: "0px",
         opacity: 0,
         duration: 0.5,
         ease: "power3.inOut",
-    }, "goUp +=0.5");
+    }, "goUp+=0.5");
+    /* If withSliding is true, move all titles up for a bit */
+    if (withSliding) {
+    timeline.to("#hero div:has(> .text-title)", {
+            y: -756,
+            duration: 2,
+            ease: "power3.inOut",
+        });
+        // return
+    }
     /* Move all titles up/down and hide them */
-    timeline.to("div.up:has(> .text-title)", {
-        y: -1000,
+    timeline.to("#hero div.up:has(> .text-title)", {
+        y: -1200,
         opacity: 0,
         duration: 1,
         ease: "power3.in",
-    }, "goUp+=0.5");
-    timeline.to("div.down:has(> .text-title)", {
-        y: 1000,
+    }, "+=0.5 phaseOut");
+    timeline.to("#hero div.down:has(> .text-title)", {
+        y: 800,
         opacity: 0,
         duration: 1,
         ease: "power3.in",
-    }, "goUp+=0.5");
-    timeline.to("div:has(> .text-title:not(.after))", {
+    }, "phaseOut-=1");
+    timeline.to("#hero div:not(.up):not(.down):has(> .text-title)", {
+        y: 0,
+        duration: 0.8,
+        ease: "linear",
+    }, "-=0.2");
+    timeline.to("#hero div:not(.stretch):has(> .text-title)", {
         maxHeight: "0px",
         opacity: 0,
         padding: "0px!important",
         duration: 1,
-        ease: "power3.inOut"
-    }, "goUp+=1");
+        ease: "linear"
+    }, "-=1");
     /* Stretch the middle title */
-    timeline.to(".text-title.stretch", {
+    timeline.to("#hero .text-title.stretch", {
         transform: "scale(1, 4)",
         duration: 1,
         ease: "power3.inOut"
-    }, "goUp+=0.75 stretchFrame");
+    }, "phaseOut-=0.75 stretchFrame");
     /* Add shadow to the stretched title */
-    timeline.to(".text-title.stretch", {
-        textShadow: "#031d15 -7px 7px 0",
+    timeline.to("#hero .text-title.stretch", {
+        textShadow: `${textShadowColor} -7px 7px 0`,
         duration: 0.5,
         ease: "power3.out"
     }, "stretchFrame-=0.5");
     /* Give a little 3d effect by skewing the title */
-    timeline.to(".text-title.stretch", {
-        transform: "scale(1, 4) skew(10deg, -0.1deg)",
-        textShadow: "#031d15 -14px 14px 0",
+    timeline.to("#hero .text-title.stretch", {
+        transform: "translateZ(0) scale(1, 4) skew(10deg, -0.1deg)",
+        textShadow: `${textShadowColor} -14px 14px 0`,
         duration: 1,
         ease: "back.out"
     }, "stretchFrame+=0");
-    timeline.to(".text-title.stretch", {
-        transform: "scale(1, 4) skew(0deg, 0deg)",
-        textShadow: "#031d15 -7px 7px 0",
+    /* Then skew the other way */
+    timeline.to("#hero .text-title.stretch", {
+        transform: "translateZ(0) scale(1, 4) skew(-10deg, 0.1deg)",
+        textShadow: `${textShadowColor} 14px 14px 0`,
+        duration: 1,
+        ease: "back.inOut"
+    }, "+=0.25");
+    /* Return to normal */
+    timeline.to("#hero .text-title.stretch", {
+        transform: "translateZ(0) scale(1, 4) skew(0deg, 0deg)",
+        textShadow: `${textShadowColor} -7px 7px 0`,
         duration: 0.5,
         ease: "power3.out"
-    }, "stretchFrame+=1");
+    }, "+=1");
+    /* flash through a bunch of fonts */
+    fontsAndColors.forEach((fontAndColor) => {
+        timeline.to("#hero .text-title.stretch", {
+            fontFamily: fontAndColor[0],
+            color: fontAndColor[1],
+            duration: 0.01,
+            ease: "power1.inOut"
+        }, "+=0.075");
+    });
+
     /* End stretch */
-    timeline.to(".text-title.stretch", {
+    timeline.to("#hero .text-title.stretch", {
         transform: "scale(1, 1)",
         duration: 1,
         ease: "back.inOut"
-    }, "+=0.1 endStretch");
-    timeline.to(".text-supertitle.after", {
+    }, "+=0.5 endStretch");
+    timeline.to("#hero .text-supertitle.after", {
         y: 0,
         duration: 1.5,
         ease: "power3.out",
         maxHeight: "73px",
     }, "endStretch+=0.5");
     /* Fade-in 'I' in Super Title */
-    timeline.to(".text-supertitle.after.self-start", {
+    timeline.to("#hero .text-supertitle.after.self-start", {
         opacity: 1,
         duration: 0.25,
         overflow: "unset",
         ease: "power3.out",
     }, "endStretch+=0.75");
     /* Add the 'TH' and 'S' to transform 'I' into 'THIS' and also add shadow */
-    timeline.to(".text-supertitle.after.self-start", {
-        textShadow: "#260043 -2px 2px 0",
-    }, "endStretch+=2");
-    timeline.to(".text-supertitle.after.self-start > :nth-child(1)", {
-        maxWidth: "250px",
-        textShadow: "#260043 -2px 2px 0",
-        duration: 2.5,
+    timeline.to("#hero .text-supertitle.after.self-start", {
+        textShadow: "rgba(245, 245, 245, 0.2) -2px 2px 0",
+        duration: 0.5,
         ease: "power3.out",
     }, "endStretch+=2");
-    timeline.to(".text-supertitle.after.self-start > :nth-child(2)", {
-        maxWidth: "80px",
-        textShadow: "#260043 -2px 2px 0",
-        duration: 1.5,
-        ease: "power3.out",
-    }, "endStretch+=2.25");
+    // timeline.to("#hero .text-supertitle.after.self-start > :nth-child(1)", {
+    //     maxWidth: "250px",
+    //     textShadow: "oklch(66.7% 0.295 322.15) -2px 2px 0",
+    //     duration: 2.5,
+    //     ease: "power3.out",
+    // }, "endStretch+=2");
+    // timeline.to("#hero .text-supertitle.after.self-start > :nth-child(2)", {
+    //     maxWidth: "80px",
+    //     textShadow: "oklch(66.7% 0.295 322.15) -2px 2px 0",
+    //     duration: 1.5,
+    //     ease: "power3.out",
+    // }, "endStretch+=2.25");
     /* Fade-in 'AM' in Super Title */
-    timeline.to(".text-supertitle.after.self-end", {
+    timeline.to("#hero .text-supertitle.after.self-end", {
         opacity: 1,
         duration: 0.25,
         ease: "power3.out",
     }, "endStretch+=1");
     /* Convert 'AM' to 'IS' */
-    timeline.to(".text-supertitle.after.self-end > span.initial", {
-        maxWidth: "0px",
-        duration: 2.5,
+    // timeline.to("#hero .text-supertitle.after.self-end > span.initial", {
+    //     maxWidth: "0px",
+    //     duration: 2.5,
+    //     ease: "power3.out",
+    // }, "endStretch+=2.4");
+    // timeline.to("#hero .text-supertitle.after.self-end > span:not(.initial)", {
+    //     maxWidth: "256px",
+    //     overflow: "unset",
+    //     textShadow: "oklch(66.7% 0.295 322.15) -2px 2px 0",
+    //     duration: 2.5,
+    //     ease: "power3.out",
+    // }, "endStretch+=2.4");
+    timeline.to("#hero .text-supertitle.after.self-end", {
+        textShadow: "rgba(245, 245, 245, 0.2) -2px 2px 0",
+        duration: 0.5,
         ease: "power3.out",
-    }, "endStretch+=2.4");
-    timeline.to(".text-supertitle.after.self-end > span:not(.initial)", {
-        maxWidth: "256px",
-        overflow: "unset",
-        textShadow: "#260043 -2px 2px 0",
-        duration: 2.5,
-        ease: "power3.out",
-    }, "endStretch+=2.4");
+    }, "endStretch+=2");
 }       
-// setupBackground();
-animateStartupAlternative();
+
+function registerScrollTrigger() {
+    // This makes the scrolling horizontal for the #scroll section
+
+    const container = document.querySelector("#scroll .inner");
+
+    horizontalTween = gsap.to("#scroll .inner", {
+        id: "horizontalScroll",
+        x: () => { return -(container.scrollWidth - document.querySelector("#scroll").offsetWidth); },
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#scroll",
+            start: "top top",
+            end: () => "+=" + (container.scrollWidth - document.querySelector("#scroll").offsetWidth),
+            scrub: 1,
+            snap: 1,
+            pin: true,
+            anticipatePin: 1,
+            invalidateOnRefresh: true,
+            onLeave: () => {
+                gsap.to("#things", { opacity: 0, duration: 1 });
+                gsap.to("#inner-supertitle", { opacity: 1, maxHeight: "73px", duration: 1 });
+            },
+            onEnterBack: () => {
+                gsap.to("#things", { opacity: 1, duration: 1 });
+                gsap.to("#inner-supertitle", { opacity: 0, maxHeight: "0px", duration: 1 });
+            },
+        }
+
+    });
+
+    // Create a separate timeline for the color changes
+    const colorTimeline = gsap.timeline({ paused: true })
+        .to("#scroll .inner", 
+            {
+                backgroundColor: "#000000",
+                color: "#FFFFFF",
+                duration: 1,
+                ease: "none"
+            }
+        );
+
+    // Control the color timeline with the main scroll progress
+    ScrollTrigger.create({
+        trigger: "#scroll",
+        start: "top top",
+        end: () => "+=" + (document.querySelector("#scroll .inner").scrollWidth - document.querySelector("#scroll").offsetWidth),
+        scrub: true,
+        onUpdate: function(self) {
+            // Start color change at 50% progress, complete at 100%
+            if (self.progress >= 0.5) {
+                const colorProgress = (self.progress - 0.5) * 2; // Map 0.5-1 to 0-1
+                colorTimeline.progress(colorProgress);
+            } else {
+                colorTimeline.progress(0); // Keep at start during first half
+            }
+        },
+    });
+
+    // Add text Shadow to #invisible when it enters the viewport
+    gsap.to("#invisible", {
+        textShadow: `${textShadowColor} -2px 2px 0`,
+        ease: "power3.out",
+        scrollTrigger: {
+            trigger: "#invisible",
+            start: "left center",
+            end: "right center",
+            scrub: true,
+            containerAnimation: horizontalTween,
+        }
+    });
+
+    // Add text colour to #work when it enters the viewport
+    gsap.to("#work", {
+        color: "#FFFFFF",
+        ease: "power3.out",
+        scrollTrigger: {
+            trigger: "#work",
+            // start: "left right",
+            end: "center center",
+            scrub: true,
+            containerAnimation: horizontalTween,
+            // markers: true,
+        }
+    });
+
+}
+
+animateStartup(true, false);
+registerScrollTrigger();
+
+window.addEventListener("resize", () => {
+    if (horizontalTween) {
+        horizontalTween.scrollTrigger.refresh();
+    }
+});
